06829e49ce8bea9f7324cf47ae2cd175d240a716
Thorn <subhrajyoti.ghosh@outlook.com>
2024-01-20 16:19:58 +0530

    initial commit

---
+++ src/test/java/org/test/springtest/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3dcdf38e06434e718e16956e59a0864c4c197ed1
Thorn <subhrajyoti.ghosh@outlook.com>
2024-01-31 12:36:47 +0530

    fixed registration issue

--- src/main/java/org/jobapplication/springbootapi/service/UserService.java
+++ src/main/java/org/jobapplication/springbootapi/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,8 +14,15 @@ public class UserService {
     private UserInfoRepository userInfoRepository;
 
     public UserInfo registerUser(String email) {
+        Optional<UserInfo> existingUser = userInfoRepository.findById(email);
+        if (existingUser.isPresent()) {
+            return null;
+        }
         UserInfo userInfo = new UserInfo();
         userInfo.setEmail(email);
+        userInfo.setTenthBoardPercent(null);
+        userInfo.setTwelfthBoardPercent(null);
+        userInfo.setCollegeCgpa(null);
         return userInfoRepository.save(userInfo);
     }
 
